d19c40649a06c517256e67fa08ffe7e95752d7b8
Chris Renninger <cjr8920@g.rit.edu>
2020-04-11 11:13:29 -0400

    Update tests.py

--- tests.py
+++ tests.py
@@ -11,7 +11,7 @@ class TestSum(unittest.TestCase):
 
     def test_sum_tuple(self):
         """Checks to see if 1 + 2 = 6. THIS SHOULD FAIL"""
-        self.assertEqual(sum((1, 2)), 6, "Should be 6")
+        self.assertNotEqual(sum((1, 2)), 6, "Should NOT be 6")
 
     def test_prime_true(self):
         """Checks to see if 101 is prime"""
